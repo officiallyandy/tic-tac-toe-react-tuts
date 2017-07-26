4b91ba29d22f971d3450a25625c6eb59444c81bf
Andy Nunez <anunez@mhelpdesk.com>
2017-07-26 16:23:02 -0400

    setting up next player

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ class Board extends React.Component {
     constructor() {
         super();
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     renderSquare(i) {
@@ -26,12 +27,15 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] =  this.state.xIsNext ? 'X' : '0';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
